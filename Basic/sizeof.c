aa79224e3340bfc95bc13b7ebcc569104fe481e9
Shubhamgharge <159037288+Shubhamgharge@users.noreply.github.com>
2024-10-07 11:28:29 +0530

    Add files via upload

---
+++ Basic/sizeof.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   
+   printf("-----------*****---------------\n");
+   
+   printf("Size of integer : %d \n" ,sizeof (int) );
+   printf("size of long int : %d \n",sizeof (long int) );
+   printf("size of short int : %d \n",sizeof (short int) );
+   printf("size of character : %d \n",sizeof (char) );
+   printf("size if float : %d \n",sizeof (float));
+   printf("size of double : %d \n",sizeof (double));
+   printf("size of void : %d \n",sizeof (void));
+   
+   printf("\n----------*****---------------");
+   return 0;
+}
