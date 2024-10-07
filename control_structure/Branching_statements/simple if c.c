fa577150c99c97630294f5e32691b38e6287e904
Shubhamgharge <159037288+Shubhamgharge@users.noreply.github.com>
2024-10-07 21:48:42 +0530

    Add files via upload

---
+++ control_structure/Branching_statements/simple if c.c
@@ -0,0 +1,23 @@
+///simple if
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+   int no=0;
+   
+   printf("-----------*****---------------\n");
+   
+   printf(" \n Enter a number : ");
+   scanf("%d",&no);
+
+   if(no > 0)
+   {
+     printf("\n Number is pisativ ");
+   }
+   
+   printf("\n\n----------*****---------------");
+   getch();
+   return 0;
+   
+}
