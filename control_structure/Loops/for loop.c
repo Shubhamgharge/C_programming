0512ce73d2e2c4ef04c31795dfed97c667e2800a
Shubhamgharge <159037288+Shubhamgharge@users.noreply.github.com>
2024-10-08 09:37:58 +0530

    Add files via upload

---
+++ control_structure/Loops/for loop.c
@@ -0,0 +1,25 @@
+///for loop
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+   int no=5,i=0;
+   
+   printf("-----------*****---------------\n");
+   
+   for(i=1 ; i<=no ; i++)
+   {
+     printf("!!..Jay Ganesh..!!\n");
+   
+   } 
+    
+     
+    printf("\n\n Thank you press any kay to exist!!");
+ 
+   printf("\n\n----------*****---------------");
+   getch();
+   return 0;
+   
+}
+    
