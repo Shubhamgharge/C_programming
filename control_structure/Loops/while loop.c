6877f04cc0b457322e8f9615ff62a26966f510ae
Shubhamgharge <159037288+Shubhamgharge@users.noreply.github.com>
2024-10-08 09:40:44 +0530

    Add files via upload

---
+++ control_structure/Loops/while loop.c
@@ -0,0 +1,24 @@
+///while  loop
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+   int no=5,i=0;
+   
+   printf("-----------*****---------------\n");
+   
+   while(i<=no)
+   {
+     printf("!!..Jay Ganesh..!!\n");
+     i++;
+   } 
+    
+    printf("\n\n Thank you press any kay to exist!!");
+ 
+   printf("\n\n----------*****---------------");
+   getch();
+   return 0;
+   
+}
+    
