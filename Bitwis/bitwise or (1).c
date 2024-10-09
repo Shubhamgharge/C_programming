041ad76f1beb4a7cf917eef8951d1ce0802a90d9
Shubhamgharge <159037288+Shubhamgharge@users.noreply.github.com>
2024-10-09 12:08:16 +0530

    Add files via upload

---
+++ Bitwis/bitwise or (1).c
@@ -0,0 +1,20 @@
+// bitewise or
+ #include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int no1=0,no2=0,res=0;
+    
+    printf("\n Enter a Number 1 : ");
+    scanf("%d",&no1);
+    printf("\n Enter a Number 2 : ");
+    scanf("%d",&no2);
+    
+    
+    printf("\n-----------------********-----------------n");
+    res =no1 | no2 ;
+    printf("\n Number %d | %d is  = %d",no1,no2,res);
+    printf("\n-----------------********-----------------\n");
+    getch();
+    return 0;
+}
