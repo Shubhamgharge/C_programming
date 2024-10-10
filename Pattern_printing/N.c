155af1bfddc710f23062f3b1160706549dbfe779
Shubhamgharge <159037288+Shubhamgharge@users.noreply.github.com>
2024-10-10 22:49:32 +0530

    Add files via upload

---
+++ Pattern_printing/N.c
@@ -0,0 +1,35 @@
+///basic N pattern
+       
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+   int s=0,i=0,j=0;
+   
+   
+   printf("\nenter row and column size=");
+   scanf("%d",&s);
+   printf("\n------------********----------------\n\n");
+   for(i=1 ; i<=s ; i++)
+   {
+      for(j=1 ; j<=s ; j++)
+       {
+           if(j==1 || i==j ||j==s )
+           {
+              printf(" * ");
+           }
+           else
+           {
+            printf("   ");
+           }
+       } 
+       printf("\n");
+   }
+    
+   
+   printf("\n-------------*******----------------\n");
+   printf("\n press any key to exit..!!");
+   getch();
+   return 0;
+}
