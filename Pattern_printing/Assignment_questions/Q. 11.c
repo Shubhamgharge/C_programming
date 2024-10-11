280ac30e19601d2e25ddadb78171818ff24d0848
Shubhamgharge <159037288+Shubhamgharge@users.noreply.github.com>
2024-10-11 21:43:29 +0530

    Add files via upload

---
+++ Pattern_printing/Assignment_questions/Q. 11.c
@@ -0,0 +1,36 @@
+//Q.11
+       
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+   int s=0,i=0,j=0;
+   
+   
+   
+   printf("\nenter row and column size=");
+   scanf("%d",&s);
+   printf("\n------------********----------------\n\n");
+   for(i=1 ; i<=s ; i++)
+   {
+      for( j=1 ; j<=s ; j++)
+       {
+           if(i>=j)
+           {
+              printf("$");
+           }
+           
+        /*  else
+           {
+            printf("   ");
+           }*/
+       } 
+       printf("\n");
+   }
+   
+   printf("\n-------------*******----------------\n");
+   printf("\n press any key to exit..!!\n\n");
+   getch();
+   return 0;
+}
