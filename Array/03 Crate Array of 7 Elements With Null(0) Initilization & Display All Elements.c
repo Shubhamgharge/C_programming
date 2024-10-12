41565007752db8539b37294141e45df3bfc3dbfd
Shubhamgharge <159037288+Shubhamgharge@users.noreply.github.com>
2024-10-12 15:31:30 +0530

    Add files via upload

---
+++ Array/03 Crate Array of 7 Elements With Null(0) Initilization & Display All Elements.c
@@ -0,0 +1,22 @@
+///03 Crate Array of 7 Elements With Null(0) Initilization & Display All Elements
+       
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   int Bill[7]={0};
+   printf("\n------------********----------------\n");
+   
+   printf("\nArray  1st element is %d",Bill[0]);
+   printf("\nArray  2nd element is %d",Bill[1]);
+   printf("\nArray  3rd element is %d",Bill[2]);
+   printf("\nArray  4th element is %d",Bill[3]);
+   printf("\nArray  5th element is %d",Bill[4]);
+   printf("\nArray  6th element is %d",Bill[5]);
+   printf("\nArray  7th element is %d",Bill[6]); 
+   
+   printf("\n\n-------------*******----------------\n");
+   printf("\n press any key to exit..!!\n\n");
+   getch();
+   return 0;
+}
