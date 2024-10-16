6f2ee7ab1020e07cc2f0e1f1f89332e7b0741bff
Shubhamgharge <159037288+Shubhamgharge@users.noreply.github.com>
2024-10-16 23:54:55 +0530

    Add files via upload

---
+++ Class_new_assignment/06 size of. c.c
@@ -0,0 +1,9 @@
+//06 size od
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+     printf("\n  size of int : %d",sizeof(int));
+     printf("\n  size of float : %d",sizeof(float));
+     printf("\n  size of double : %d",sizeof(double));
+}
