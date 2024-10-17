8f18c9a09fb60976135545d204b515a31c0d8519
Shubhamgharge <159037288+Shubhamgharge@users.noreply.github.com>
2024-10-17 21:28:49 +0530

    Delete Class_new_assignment/06 size of. c.c

--- Class_new_assignment/06 size of. c.c
+++
@@ -1,9 +0,0 @@
-//06 size od
-#include<stdio.h>
-#include<conio.h>
-int main()
-{
-     printf("\n  size of int : %d",sizeof(int));
-     printf("\n  size of float : %d",sizeof(float));
-     printf("\n  size of double : %d",sizeof(double));
-}
