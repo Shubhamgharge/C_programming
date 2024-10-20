0dbd7b915ea0e2e758bbc1caf7e3d480263157f1
Shubhamgharge <159037288+Shubhamgharge@users.noreply.github.com>
2024-10-20 22:18:15 +0530

    Add files via upload

---
+++ Class_new_assignment/4_digit_assignment/06 differe bitween sum of even & odd digit count.c
@@ -0,0 +1,44 @@
+//06 differe bitween sum of even & odd digit count
+#include<stdio.h>
+#include<conio.h>
+int difference(int N1)
+{
+    int itemp=0, idig=0, ecount=0, ocount=0, esum=0, osum=0, diff=0;
+    itemp=N1;
+    
+     while(itemp > 0)
+     {
+        idig= itemp % 10;
+        
+        if((idig % 2 == 0) && (idig !=0))
+        {
+           esum=esum+idig;
+        }
+       if(idig % 2 == 1) 
+        {
+           osum=osum+idig; 
+           
+        }
+        
+        itemp=itemp/10;
+     }
+       
+     diff= esum - osum;
+     
+    
+    printf("\n difference between sum of even count and sum of odd count (%d - %d)= %d ",esum, osum,diff);
+     
+}
+int main()
+{
+     int iNum=0 ;
+     
+     printf("\n Enter a Number to count even digit count : ");
+     scanf("%d",&iNum);
+
+     difference(iNum);
+      
+     
+     getch();
+     return 0;
+}
