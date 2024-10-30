7205b8f68d7b40ae8224f4ba20c97bab7246fe9b
Shubhamgharge <159037288+Shubhamgharge@users.noreply.github.com>
2024-10-30 23:02:22 +0530

    Add files via upload

---
+++ Class_new_assignment/5_Assignment/01 Array without Initalization.c
@@ -0,0 +1,17 @@
+//01 Create Array Without Initialization & Display All Elements
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{ 
+    int iAge[5];
+    
+    printf("\n 1st Array Element is %d.",&iAge[0]);
+    printf("\n 2nd Array Element is %d.",&iAge[1]);
+    printf("\n 3rd Array Element is %d.",&iAge[2]);
+    printf("\n 4th Array Element is %d.",&iAge[3]);
+    printf("\n 5th Array Element is %d.",&iAge[4]);
+    
+   getch();
+   return 0;
+}
