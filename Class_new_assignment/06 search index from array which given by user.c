510967dc24c3c575c76c4d248b47749eead7bd33
Shubhamgharge <159037288+Shubhamgharge@users.noreply.github.com>
2024-10-30 23:04:34 +0530

    Add files via upload

---
+++ Class_new_assignment/06 search index from array which given by user.c
@@ -0,0 +1,45 @@
+//06 Create, Accept, Display Array Using Loop - with macro, 
+//Search Given Element in that Array & Return its Index If Found.
+#include<stdio.h>
+#include<conio.h>
+#define size 7
+int main()
+{ 
+    int i=0, iAge[size]={0}, iSearch=0;
+   
+    printf("\n\nAccept %d array element.",size);
+    
+    for(i=0 ; i<size ; i++)
+    {
+      printf("\nEnter %d array element : ",i); 
+      scanf("%d",&iAge[i]);
+    }
+   
+   printf("\n\nDisplay all elements :");
+   
+   printf("\n\n Enter number to search in the array : ");
+   scanf("%d",&iSearch);
+   for(i=0 ; i<size ; i++)
+   {
+      if(iSearch ==iAge[i]) 
+      {
+         //printf("\n %d found at %d index",iSearch,i);
+         break;
+        //return true;
+      }
+   }   
+      if(i<size)
+      {
+         printf(" \n%d found bat %d index",iSearch,i);
+      }
+      else 
+      {
+        printf("\n %d  not found",iSearch);
+        //return false;
+      }
+    
+   getch();
+   return 0;
+   
+}
+    
